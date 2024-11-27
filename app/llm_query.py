15709108b2bc877d6b3b0967e21833e72c1bccf8
tanaymurdia <tanay.itd@gmail.com>
2024-11-27 18:20:18 -0500

    Version 0.11

--- app/llm_query.py
+++ app/llm_query.py
@@ -1,8 +1,10 @@
 from openai import OpenAI
 import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 api_key = os.getenv('OPENAI_API_KEY')
-print(api_key)
 
 def conversation_to_string(conversation):
     # print(conversation)
